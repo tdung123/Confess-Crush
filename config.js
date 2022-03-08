ddb99f49b80aff63bd2fc134201f78b80a54e535
tdung123 <92414597+tdung123@users.noreply.github.com>
2022-03-08 15:09:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,18 @@ const CONFIG = {
     titleWeb: "Gửi tới chị yêu của Dũn",
     introTitle: 'Phan Thu Hiền',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao chị cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm ❤',
+    btnNo: 'Không nha 🤣',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em ❤',
+    btnReply: 'Gửi cho em yêu ❤',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ❤❤❤',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
     messDesc: 'Chị làm người yêu em nhé',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+   }
