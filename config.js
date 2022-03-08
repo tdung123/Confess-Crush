b2f1c3b3c3c2cdbc4032183409940a703b6a3519
tdung123 <92414597+tdung123@users.noreply.github.com>
2022-03-08 15:11:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy 🥰',
     btnYes: 'Thích lắm ❤',
     btnNo: 'Không nha 🤣',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em ❤',
